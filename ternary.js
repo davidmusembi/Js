2ca3bf80b01556107c68fb8dece2d9bbffa35f28
David Musembi <davidmusembi02@gmail.com>
2022-03-15 20:13:14 +0300

    Find Even and Odd numbers

--- ternary.js
+++ ternary.js
@@ -1,11 +1,15 @@
 //Find number is even or odd
- let num =9;
+ let num = prompt("Enter a number");
  let result;
- if(num%2){
-        result="odd";
-        console.log("Number is" + result)
+ if(num%2 ===0){
+        result="even";
+        console.log("Number is "  + result)
  }
  else{
-     result="even";
+     result="odd";
      console.log("Number is " + result)
     }
+                  //OR
+    //using ?:
+    result = num%2 === 0 ? "even" : "odd";
+    console.log(result);
