dc622de9f43120a198175938bdbee06bc7fde333
David Musembi <davidmusembi02@gmail.com>
2022-03-15 20:38:18 +0300

    Use of templete literal

--- logical.js
+++ logical.js
@@ -1,14 +1,20 @@
 // //Using AND operator && operator
 // //In AND operation if both condition are true you get true
-// let x=7 ,y=8 ,z=5 ;
-// console.log(x<y && x<z);
+let x=7 ,y=8 ,z=5 ;
+console.log(x<y && x<z);
 
 // //Using OR operator || operator
 // //In OR operation if one of the condition is true you get true
-// console.log(x<y || x>z);
+console.log(x<y || x>z);
 
 // //Using NOT operator ! operator
 // //In NOT operator if one of the condition is false you get true
-// console.log(!(x>y));
+console.log(!(x>y));
+
+//Use of Template Literals
+ let name = "John";
+    let age = 15;
+    console.log(`My Name is ${name} I am ${age} years old `)
+
 
 
