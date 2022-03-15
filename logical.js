61801d498be3f2d3c4368a9aed18389a739af90d
David Musembi <davidmusembi02@gmail.com>
2022-03-15 14:56:21 +0300

    Find even number using conditional statement

--- logical.js
+++ logical.js
@@ -1,12 +1,14 @@
-//Using AND operator && operator
-//In AND operation if both condition are true you get true
-let x=7 ,y=8 ,z=5 ;
-console.log(x<y && x<z);
+// //Using AND operator && operator
+// //In AND operation if both condition are true you get true
+// let x=7 ,y=8 ,z=5 ;
+// console.log(x<y && x<z);
 
-//Using OR operator || operator
-//In OR operation if one of the condition is true you get true
-console.log(x<y || x>z);
+// //Using OR operator || operator
+// //In OR operation if one of the condition is true you get true
+// console.log(x<y || x>z);
 
-//Using NOT operator ! operator
-//In NOT operator if one of the condition is false you get true
-console.log(!(x>y));
+// //Using NOT operator ! operator
+// //In NOT operator if one of the condition is false you get true
+// console.log(!(x>y));
+
+
---
+++ ternary.js
@@ -0,0 +1,11 @@
+//Find number is even or odd
+ let num =9;
+ let result;
+ if(num%2){
+        result="odd";
+        console.log("Number is" + result)
+ }
+ else{
+     result="even";
+     console.log("Number is " + result)
+    }
